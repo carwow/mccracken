d56ea34febac0277e2400a9859fabf87900d091c
Jonathan Gnagy <jonathan.gnagy@gmail.com>
2017-05-03 10:52:59 -0700

    Adding some ActiveRecord compatibility methods

---
+++ lib/mccracken/compat/active_record.rb
@@ -0,0 +1,42 @@
+# ActiveRecord ActiveModel::Name compatibility methods
+class McCracken::Resource
+  def self.human
+    i18n_key.humanize
+  end
+
+  def self.i18n_key
+    self.name.split('::').last.underscore
+  end
+
+  def self.param_key
+    singular_route_key.to_sym
+  end
+
+  def self.route_key
+    singular_route_key.en.plural
+  end
+
+  def self.singular_route_key
+    self.name.split('::').last.underscore
+  end
+
+  def model_name
+    self.class
+  end
+
+  def new?
+    id ? false : true
+  end
+
+  def to_model
+    self
+  end
+
+  def to_key
+    new? ? [] : [id]
+  end
+
+  def to_param
+    new? ? nil : id.to_s
+  end
+end
